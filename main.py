c7e06b9238d32e7f96ad8ab7cd45d5b365a00973
nesiv3 <83553350+nesiv3@users.noreply.github.com>
2025-05-13 16:09:48 -0500

    Ajustes swagger

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ from fastapi import FastAPI
 from infraestructure.database import Base, engine
 from api import store
 
-app = FastAPI()
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="BikeWork Store API",
+    description="API para gestionar las tiendas de BikeWork.",
+    version="1.0.0",
+    contact={
+        "name": "Soporte BikeWork",
+        "email": "soporte@bikework.com",
+    },
+    license_info={
+        "name": "DasavaNIV License",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+)
+
+
 
 Base.metadata.create_all(bind=engine)
 
